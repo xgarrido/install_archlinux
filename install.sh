ea93f169e7620a3bcad7fb9b3f739d5682fc3611
Xavier Garrido <xavier.garrido@gmail.com>
2019-12-01 09:34:47 +0100

    first version

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+banner="\\033[0;34m\n
+\n
+##############################################\n
+# Installation program for fresh arch linux  #\n
+##############################################\n
+\\033[0;39m\n
+"
+echo -ne ${banner}
+
+function msg_error()
+{
+    echo -en "\\033[0;31mERROR: $@\\033[0;39m\n"
+    return 0
+}
+
+function msg_notice()
+{
+    echo -en "\\033[0;34mNOTICE: $@\\033[0;39m\n"
+    return 0
+}
+
+function msg_warning()
+{
+    echo -en "\\033[0;35mWARNING: $@\\033[0;39m\n"
+    return 0
+}
+
+msg_notice "Testing existence of 'pacman'"
+which pacman > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    msg_error "Missing 'pacman' binary!"
+    exit 1
+fi
+
+msg_notice "Installing 'base-devel' and 'git'"
+sudo pacman -Sy --noconfirm --needed base-devel git
+
+msg_notice "Installing 'yaourt'"
+(
+    cd $(mktemp -d)
+    git clone https://aur.archlinux.org/package-query.git
+    cd package-query
+    makepkg -si --noconfirm
+    cd ..
+    git clone https://aur.archlinux.org/yaourt.git
+    cd yaourt
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf $(pwd)
+)
